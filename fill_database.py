95ba6978ed3013a273121af3a42e57010869cca4
mkunissery <kpmkrishnan@gmail.com>
2018-05-20 20:32:26 -0400

    modified update to mongo to improve performance. correlation service will be added to main file

--- fill_database.py
+++ fill_database.py
@@ -4,33 +4,54 @@ from bson import json_util
 from collections import namedtuple
 from pymongo import MongoClient
 import itertools
-
+from bson.json_util import dumps
+from datetime import datetime
+import pandas as pd
 db = None
 
+class CryptoDBUtil():
 
+    def __init__(self):
+        pass
 
-def coinList():
-    res = requests.get('https://min-api.cryptocompare.com/data/all/coinlist')
-    res_json = [x for x in res.json()['Data'].values() if int(x['SortOrder']) < 15]
-    newlist = sorted(res_json, key=lambda k: k['SortOrder'])
-    currencies = []
-    for item in newlist:
-        currencies.append(item['Name'])
-    #print(currencies)
-    pairs = list(itertools.combinations(currencies, 2))
-    for pair in pairs:
-        res = requests.get("https://min-api.cryptocompare.com/data/histoday?fsym="+pair[0]+"&tsym="+pair[1]+"&limit=730")
-        result = json.loads(res.text)
+    def coinList(self):
+        print(datetime.now())
         client = MongoClient(port=27017)
-        db = client.final
-        for item in result['Data']:
-            result = db.coins.update_one({"FROM": pair[0], "TO": pair[1], 'time': item.get('time')},
-            {'$set': {'close': item.get('close'),
-            'high': item.get('high'),
-            'low': item.get('low'),
-            'open': item.get('open'),
-            'volumefrom': item.get('volumefrom'),
-            'volumeto': item.get('volumeto')}}, True)
+        db = client.finals
+
+        rec = db.coins.find()
+        if rec.count() > 0:
+            db.drop_collection('coins')
+
+        dfResult = pd.DataFrame()
+        res = requests.get('https://min-api.cryptocompare.com/data/all/coinlist')
+        res_json = [x for x in res.json()['Data'].values() if int(x['SortOrder']) < 15]
+        newlist = sorted(res_json, key=lambda k: k['SortOrder'])
+        currencies = []
+        for item in newlist:
+            print("Removing existing collection..")
+            currencies.append(item['Name'])
+        #print(currencies)
+
+        pairs = list(itertools.combinations(currencies, 2))
+        i = 0
+        for pair in pairs:
+            print("processing..." + pair[0] + "-" + pair[1])
+            res = requests.get("https://min-api.cryptocompare.com/data/histoday?fsym="+pair[0]+"&tsym="+pair[1]+"&limit=730")
+            df = pd.DataFrame(res.json()['Data'])
+            df1 = df[['time', 'close']]
+            df1.columns = ['time', pair[0] + "-" + pair[1]]
+            if i==0:
+                dfResult = df1
+                i = i + 1  #not required after i > 0
+            else:
+                dfResult = pd.merge(dfResult, df1, how='inner', on='time')
+        records = json.loads(dfResult.T.to_json()).values()
+        db.coins.insert(records)
+        print(datetime.now())
+        return True
 
 
-coinList()
+cobj = CryptoDBUtil()
+cobj.coinList()
+
