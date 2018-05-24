4ca08acc196cc1d55008ae655ec0fd9de7e80bde
mkunissery <kpmkrishnan@gmail.com>
2018-05-23 21:03:22 -0400

    added moving correlation

--- fill_database.py
+++ fill_database.py
@@ -15,47 +15,6 @@ class CryptoDBUtil():
     def __init__(self):
         pass
 
-    def coinList(self):
-        print(datetime.now())
-        client = MongoClient(port=27017)
-        db = client.finals
-
-        rec = db.coins.find()
-        if rec.count() > 0:
-            print("Removing existing collection..")
-            db.drop_collection('coins')
-
-        dfResult = pd.DataFrame()
-        res = requests.get('https://min-api.cryptocompare.com/data/all/coinlist')
-        res_json = [x for x in res.json()['Data'].values() if int(x['SortOrder']) < 15]
-        newlist = sorted(res_json, key=lambda k: k['SortOrder'])
-        currencies = []
-        for item in newlist:
-            currencies.append(item['Name'])
-        #print(currencies)
-
-        pairs = list(itertools.combinations(currencies, 2))
-        i = 0
-        for pair in pairs:
-            print("processing..." + pair[0] + "-" + pair[1])
-            res = requests.get("https://min-api.cryptocompare.com/data/histoday?fsym="+pair[0]+"&tsym="+pair[1]+"&limit=730")
-            df = pd.DataFrame(res.json()['Data'])
-            #print(df)
-            df1 = df[['time', 'open', 'close', 'volumeto', 'volumefrom']]
-            df1.columns = ['time', pair[0] + "-" + pair[1]+" open", pair[0] + "-" + pair[1]+" close", pair[0] + "-" + pair[1]+' volumeto', pair[0] + "-" + pair[1]+" volumefrom"]
-            if i==0:
-                dfResult = df1
-                i = i + 1  #not required after i > 0
-            else:
-                dfResult = pd.merge(dfResult, df1, how='inner', on='time')
-        #print(dfResult)
-        #print(dfResult.T)
-        #print(dfResult.T.to_json())
-        records = json.loads(dfResult.T.to_json()).values()
-        db.coins.insert(records)
-        print(datetime.now())
-        return True
-
     def coinFiatList(self):
         print(datetime.now())
         client = MongoClient(port=27017)
@@ -127,7 +86,5 @@ class CryptoDBUtil():
 
         return True
 
-
 cobj = CryptoDBUtil()
-#cobj.coinList()
 cobj.coinFiatList()
